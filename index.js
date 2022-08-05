00c60a6bf0e4c07ba60bca0629695d86d0377054
sachieko <kirazetsubou@gmail.com>
2022-08-04 20:59:43 -0600

    Implemented manual test for fetchISSFlyOverTimes.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./fetchMyIP');
 
+const object = { latitude: '49.2760', longitude: '-123.13000' };
+
+fetchISSFlyOverTimes(object, (err, times) => {
+  if (err) console.log('AAAAAAGH ', err);
+  console.log(times);
+});
+
 /*
 fetchCoordsByIP('NOT PUTTING MY IP UP THERE HAHA', (error, coord) => {
   if (error) {
